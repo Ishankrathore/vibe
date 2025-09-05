d24e9c254ec1e78d7228355b85aa2e6aef011f5e
Ishankrathore <148578167+Ishankrathore@users.noreply.github.com>
2025-09-05 22:18:12 +0530

    05-AI-jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,23 @@
 "use client"
 import { Button } from '@/components/ui/button';
-import React from 'react';
+import React, { useState } from 'react';
 import { useTRPC } from '@/trpc/client'; 
 import { useMutation } from '@tanstack/react-query';
+import { Input } from '@/components/ui/input';
+import { toast } from 'sonner';
 const page =  () => {
+  const [value,setValue]=useState('');
   const trpc = useTRPC();
-  const invoke=useMutation(trpc.invoke.mutationOptions({}));
+  const invoke=useMutation(trpc.invoke.mutationOptions({
+    onSuccess: ()=> {
+      toast.success('Inngest function invoked successfully');
+    }
+  }));
   return (
-    <div>
-
-      <Button onClick={()=>{
-        invoke.mutate({text:'test'})
+    <div className='p-4 max-w-7xl mx-auto'>
+      <Input value={value} onChange={(e)=>setValue(e.target.value)} />
+      <Button disabled={invoke.isPending} onClick={()=>{
+        invoke.mutate({value:value})
       }}>Invoke Inngest Function
 
       </Button>
--- src/inngest/function.ts
+++ src/inngest/function.ts
@@ -1,10 +1,18 @@
+import {openai,createAgent} from "@inngest/agent-kit"
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+    const summarizer = createAgent({
+      name: "Summarizer",
+      system: "You are a helpful assistant that summarizes text.",
+      model:openai({model:"gpt-4o"}),
+    });
+    const {output}= await summarizer.run(`summarize the following text:${event.data.value}`);
+    //console.log("Summary:", output);
+    //return { message: `Hello ${event.data.value}!` };
+    return {output}
   },
 );
